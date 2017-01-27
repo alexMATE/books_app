201eda2458dc9d295c0acc4b711ff2509495defe
Alex Man <alexman.devtools@gmail.com>
2017-01-27 16:40:36 +0200

    First commit of book app

---
+++ add_data.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Add Books</title>
+</head>
+<body>
+  <div id="header">
+ <div id="content">
+    <label>CRUD Operations With PHP and MySql - By Cleartuts</label>
+    </div>
+</div>
+<div id="body">
+ <div id="content">
+    <form method="post">
+    <table align="center">
+    <tr>
+    <td align="center"><a href="index.php">back to main page</a></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="user_name" placeholder="Your First and last name" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="book" placeholder="Book Name" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="author" placeholder="Author Name" required /></td>
+    </tr>
+    <tr>
+    <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
+    </tr>
+    </table>
+    </form>
+    </div>
+</div>
+</body>
+</html>
